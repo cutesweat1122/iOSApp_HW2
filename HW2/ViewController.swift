ff49ac1f0ac5bd0ccfeb816462925e04b23a2920
Anny <cutesweat1122@gmail.com>
2023-03-13 14:56:22 +0800

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by 蘇子涵 on 2023/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
